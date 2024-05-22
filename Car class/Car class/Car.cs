97e5ec0c00bdfff0ae246e9c1f3ec698c01e991d
Alizamin23 <alizaminam@code.edu.az>
2024-05-22 04:26:57 +0400

    added Task 22.05

---
+++ Car class/Car class/Car.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Car_class
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+
+
+        public double Drive(int km)
+        
+        {
+            double requiredFuel = km * FuelFor1Km;
+
+            if (CurrentFuel >= requiredFuel)
+            {
+                
+                
+                Console.WriteLine($"New traveled {km} km. Current Fuel: {CurrentFuel -= requiredFuel} liters. New Millage {Millage+=km}");
+                return CurrentFuel ;
+            }
+
+            else
+            {
+                Console.WriteLine($"Not enough fuel. Maximum distance: {CurrentFuel / FuelFor1Km} km.");
+                return CurrentFuel;
+            }
+            
+             
+        }
+    }
+}
---
+++ Car class/Car class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Car_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Brand = "Chevrolet";
+            car.Model = "Aveo";
+            car.CurrentFuel = 23;
+            car.FuelFor1Km = 0.06;
+            car.Millage = 279000;
+
+            Console.WriteLine(car.Drive(383));       
+        
+        }
+         
+    }
+}
